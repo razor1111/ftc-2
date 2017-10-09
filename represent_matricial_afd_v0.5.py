ca8be6ca50ac8bc6888a631f9533a2be2ed3e8bc
Thiago Santos Borges <31315184+thiagos98@users.noreply.github.com>
2017-10-08 23:23:45 -0400

    chore: adicionado alguns testes de validação

--- represent_matricial_afd_v0.5.py
+++ represent_matricial_afd_v0.5.py
@@ -5,10 +5,21 @@ class Array:
 		self.initial = initial
 		self.final = final
 		self.delta = delta
+		self.array_pi = []
+		self.array_final = []
+		self.array_a = []
+		self.array_b = []
 	
+	def get_array_pi(self):
+		return self.array_pi
+	def get_array_final(self):
+		return self.array_final
+	def get_array_a(self):
+		return self.array_a
+	def get_array_b(self):
+		return self.array_b	
 
 	def create_array_pi(self):
-		array_pi = []
 		element = 1
 		new_row = []
 		for i in range(self.row):
@@ -17,53 +28,47 @@ class Array:
 			else:
 				element = 0
 			new_row.append(element)
-		array_pi.append(new_row)
-		return array_pi
+		self.array_pi.append(new_row)
 
 	def create_array_final(self):
-		array_final = []
 		element = 0
 		for i in range(self.row):
 			row = []
 			for j in range(1):
 				row.append(0)
-			array_final.append(row)
+			self.array_final.append(row)
 		
 		for numb in self.final:
-			array_final[numb][0] = 1;
+			self.array_final[numb][0] = 1;
 		
-		return array_final
 
 	def create_array_a(self):
-		array_a = []
 		for i in range(self.row):
 			row = []
 			for j in range(self.column):
 				row.append(0)
-			array_a.append(row)
+			self.array_a.append(row)
 
 		for i in range(self.row):
 			for j in range(self.column):
-				if 0 == delta[i][j]:
-					array_a[i][j] = 1
+				if 0 == self.delta[i][j]:
+					self.array_a[i][j] = 1
 
-		return array_a
+
 
 	def create_array_b(self):
-		array_b = []
 		for i in range(self.row):
 			row = []
 			for j in range(self.column):
 				row.append(0)
-			array_b.append(row)
+			self.array_b.append(row)
 
 
 		for i in range(self.row):
 			for j in range(self.column):
 				if 1 == delta[i][j]:
-					array_b[i][j] = 1
+					self.array_b[i][j] = 1
 
-		return array_b
 
 def multiply_arrays(array_1,array_2):
 	result = []
@@ -87,29 +92,44 @@ def separate_word(word):
 		char.append(word[i])
 	return char
 
+def computar_palavra(array,char):
+	print('ola')
+
+def computar_resultado(array,array_char):
+	parcela1 = multiply_arrays(array.get_array_pi(),array_char)
+	resultado_computacao = multiply_arrays(parcela1,array.get_array_final())
+	return resultado_computacao[0][0]
+	
+def verificar_final(final):
+	cont = 0
+	for i in range(len(final)):
+		if final[i] < states and final[i] >= 0:
+			cont += 1
+	if cont != len(final):
+		SystemExit
 
 states = 2
 initial = 0
-final = [0]
+final = [1]
 delta = [[0,1],[0,1]]
-word = 'aabaab'
+word = 'aabbab'
+char = separate_word(word)
 
-"""
-if len(final) != 0:
+verificar_final(final)
+
+if len(final) != 0 and (initial < states and initial >= 0) and len(delta) == states:
 	array_1 = Array(states,2,initial,final,delta)
-	array_pi = array_1.create_array_pi()
-	array_final = array_1.create_array_final()
-	array_a = array_1.create_array_a()
-	array_b = array_1.create_array_b()
+	array_1.create_array_pi()
+	array_1.create_array_final()
+	array_1.create_array_a()
+	array_1.create_array_b()
 
-	result = multiply_arrays(array_b,array_final)
+	
+	resultado = computar_resultado(array_1,[[0,1],[0,1]])
+	if resultado:
+		print('ACEITA')
+	elif not(resultado):
+		print('REJEITA')
 
-	print(array_pi)
-	print(array_final)
-	print(array_a)
-	print(array_b)
-
-	print('\n')
-	print(result)
 else:
-	print('REJEITA')"""
+	print('REJEITA')
